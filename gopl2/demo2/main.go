2c5ddc09b0247ae4881f32703ab0d7f5f1dba4e8
吴磊 <wl@zcmlc.com>
2018-10-15 13:22:08 +0800

    2

--- gopl2/demo2/main.go
+++ gopl2/demo2/main.go
@@ -1,22 +1,36 @@
+// Clock1 is a TCP server that periodically writes the time.
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"io"
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
 	}
-	// NOTE: ignoring potential errors from input.Err()
-	for line, n := range counts {
-		if n > 1 {
-			// fmt.Printf("%d\t%s\n", n, line)
-			fmt.Println(1111, line, n)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
 		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
