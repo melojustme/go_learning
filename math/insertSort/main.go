29b59fb8079837f99dbbb5b11443011134f54dea
吴磊 <wl@zcmlc.com>
2018-10-22 14:50:25 +0800

    1

--- math/insertSort/main.go
+++ math/insertSort/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	a := []int{4, 5, 6, 3, 2, 1}
-	InsertSort(a)
-	fmt.Println(a)
+	// a := []int{4, 5, 6, 3, 2, 1}
+	// InsertSort2(a)
+	// fmt.Println(a)
+	a := 15
+	GuessingGame(a)
 }
 
 func InsertSort(a []int) {
@@ -27,4 +30,21 @@ func InsertSort(a []int) {
 		}
 		a[j+1] = v
 	}
+}
+
+func InsertSort2(a []int) {
+	sort.Ints(a)
+}
+func SortSearch(a []int) {
+	// sort.Search()
+}
+func GuessingGame(a int) {
+	var s string
+	fmt.Printf("Pick an integer from 0 to 100.\n")
+	answer := sort.Search(a, func(i int) bool {
+		fmt.Printf("Is your number <= %d? ", i)
+		fmt.Scanf("%s", &s)
+		return s != "" && s[0] == 'y'
+	})
+	fmt.Printf("Your number is %d.\n", answer)
 }
