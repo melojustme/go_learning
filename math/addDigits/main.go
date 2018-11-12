353729927123ccbe277e408ee861ac7a681a7c7f
吴磊 <wl@zcmlc.com>
2018-11-12 13:17:47 +0800

    better way

--- math/addDigits/main.go
+++ math/addDigits/main.go
@@ -41,3 +41,13 @@ func pow(x, n int) int {
 	}
 	return ret
 }
+
+// func addDigits(num int) int {
+// 	if num < 10 {
+// 		return num
+// 	} else if num%9 == 0 {
+// 		return 9
+// 	} else {
+// 		return num % 9
+// 	}
+// }
